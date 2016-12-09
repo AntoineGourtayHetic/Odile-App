dedc8a87151f737b4dc5559128db4a284a3d53fa
commit-master <paul.devops@gmail.com>
2016-12-09 12:23:55 +0100

    [UPDATE:2] Added accelerometer feature

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,53 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { DeviceMotion } from 'ionic-native';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private lastX:number;
+  private lastY:number;
+  private lastZ:number;
+  private moveCounter:number = 0;
+  constructor (private navController: NavController, platform:Platform) {
+    platform.ready().then(() => {
+      var subscription = DeviceMotion.watchAcceleration({frequency:200}).subscribe(acc => {
+        console.log(acc);
 
-  constructor(public navCtrl: NavController) {
+        if(!this.lastX) {
+          this.lastX = acc.x;
+          this.lastY = acc.y;
+          this.lastZ = acc.z;
+          return;
+        }
+
+        let deltaX:number, deltaY:number, deltaZ:number;
+        deltaX = Math.abs(acc.x-this.lastX);
+        deltaY = Math.abs(acc.y-this.lastY);
+        deltaZ = Math.abs(acc.z-this.lastZ);
+
+        if(deltaX + deltaY + deltaZ > 3) {
+          this.moveCounter++;
+        } else {
+          this.moveCounter = Math.max(0, --this.moveCounter);
+        }
+
+        if(this.moveCounter > 2) {
+          console.log('SHAKE');
+          this.moveCounter=0;
+        }
+
+        this.lastX = acc.x;
+        this.lastY = acc.y;
+        this.lastZ = acc.z;
+
+      });
+
+      console.log(subscription);
+
+    });
 
   }
-
 }
