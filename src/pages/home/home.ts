cf39aac87f1fe9b9614a9b4bdd10b9216b75173d
commit-master <paul.devops@gmail.com>
2016-12-09 15:22:28 +0100

    [UPDATE:2] Added socket.io sending pos info to server

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { DeviceMotion } from 'ionic-native';
 import { NavController, Platform } from 'ionic-angular';
+import * as io from "socket.io-client";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  private lastX:number;
-  private lastY:number;
-  private lastZ:number;
+  public lastX:number;
+  public lastY:number;
+  public lastZ:number;
+  private socketHost:string;
+  private socket:any;
+  private pos:Object;
   private moveCounter:number = 0;
   constructor (private navController: NavController, platform:Platform) {
     platform.ready().then(() => {
+      this.socketHost = "http://10.33.1.82:1337";
+      this.socket = io(this.socketHost);
       var subscription = DeviceMotion.watchAcceleration({frequency:200}).subscribe(acc => {
-        console.log(acc);
 
         if(!this.lastX) {
           this.lastX = acc.x;
@@ -39,13 +44,13 @@ export class HomePage {
           this.moveCounter=0;
         }
 
-        this.lastX = acc.x;
-        this.lastY = acc.y;
-        this.lastZ = acc.z;
+        this.lastX = Math.round(acc.x * 100) / 100;
+        this.lastY = Math.round(acc.y * 100) / 100;
+        this.lastZ = Math.round(acc.z * 100) / 100;
 
+        this.pos = {x: this.lastX, y: this.lastY, z: this.lastZ};
+        this.socket.emit('mobile:position', this.pos);
       });
-
-      console.log(subscription);
 
     });
 
