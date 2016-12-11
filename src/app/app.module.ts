ab1fd289580abf395a6cee7416f21d3fdbc65e9f
Antoine Gourtay <antoine.gourtay@hetic.net>
2016-12-11 15:57:07 +0100

    [PATCH:1]Patching errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { Chapter1Page } from '../pages/chapter1/chapter1';
+import { Chapter2Page } from '../pages/chapter2/chapter2';
+import { Chapter3Page } from '../pages/chapter3/chapter3';
+import { ConnectMobilePage } from '../pages/connect-mobile/connect-mobile';
+import { ElementChoicePage } from '../pages/element-choice/element-choice';
+import { HomePagePage } from '../pages/home-page/home-page';
+import { LoadingPagePage } from '../pages/loading-page/loading-page';
+import { TestTelephonePage } from '../pages/test-telephone/test-telephone';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
+    Chapter1Page,
+    Chapter2Page,
+    Chapter3Page,
+    ConnectMobilePage,
+    ElementChoicePage,
+    HomePagePage,
+    LoadingPagePage,
+    TestTelephonePage,
     TabsPage
   ],
   imports: [
@@ -17,6 +31,14 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    Chapter1Page,
+    Chapter2Page,
+    Chapter3Page,
+    ConnectMobilePage,
+    ElementChoicePage,
+    HomePagePage,
+    LoadingPagePage,
+    TestTelephonePage,
     TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
-import { HomePage } from '../home/home';
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { HomePagePage } from '../home-page/home-page';
+import { LoadingPagePage } from '../loading-page/loading-page';
+import { ElementChoicePage } from '../element-choice/element-choice';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,9 +10,9 @@ import { ContactPage } from '../contact/contact';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
-  tab3Root: any = ContactPage;
+  tab1Root: any = HomePagePage;
+  tab2Root: any = LoadingPagePage;
+  tab3Root: any = ElementChoicePage;
 
   constructor() {
 
