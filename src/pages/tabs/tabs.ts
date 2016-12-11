f5392de69d775deffda47bb8540efd0f835abcce
Antoine Gourtay <antoine.gourtay@hetic.net>
2016-12-11 17:18:18 +0100

    [PATCH:2]Patching tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { HomePagePage } from '../home-page/home-page';
 import { LoadingPagePage } from '../loading-page/loading-page';
 import { ElementChoicePage } from '../element-choice/element-choice';
+import { Chapter1Page } from '../chapter1/chapter1';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,6 +14,7 @@ export class TabsPage {
   tab1Root: any = HomePagePage;
   tab2Root: any = LoadingPagePage;
   tab3Root: any = ElementChoicePage;
+  tab4Root: any = Chapter1Page;
 
   constructor() {
 
