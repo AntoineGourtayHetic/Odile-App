45f69802fe17100422e210b28af8a350acbe7e49
Antoine Gourtay <antoine.gourtay@hetic.net>
2016-12-12 15:45:58 +0100

    [PATCH:5]Removing useless folders & Modifying loading template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { ConnectMobilePage } from '../pages/connect-mobile/connect-mobile';
 import { HomePagePage } from '../pages/home-page/home-page';
-import { LoadingPagePage } from '../pages/loading-page/loading-page';
-import { TestTelephonePage } from '../pages/test-telephone/test-telephone';
 import { ElementChoiceCloudPage } from '../pages/element-choice-cloud/element-choice-cloud';
 import { TabsPage } from '../pages/tabs/tabs';
 import {ElementChoiceMoonPage} from "../pages/element-choice-moon/element-choice-moon";
@@ -13,10 +10,7 @@ import { ElementChoiceGroundPage } from '../pages/element-choice-ground/element-
 @NgModule({
   declarations: [
     MyApp,
-    ConnectMobilePage,
     HomePagePage,
-    LoadingPagePage,
-    TestTelephonePage,
     TabsPage,
     ElementChoiceCloudPage,
     ElementChoiceMoonPage,
@@ -28,10 +22,7 @@ import { ElementChoiceGroundPage } from '../pages/element-choice-ground/element-
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    ConnectMobilePage,
     HomePagePage,
-    LoadingPagePage,
-    TestTelephonePage,
     TabsPage,
     ElementChoiceCloudPage,
     ElementChoiceMoonPage,
