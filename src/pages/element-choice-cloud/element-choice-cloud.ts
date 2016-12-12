7941a676d64a8fd9dccd5ea005b3ac7eeee602a7
Aude <odefi@hotmail.fr>
2016-12-12 19:23:58 +0100

    [add:3] animation on next game button without the js trigger

--- src/pages/element-choice-cloud/element-choice-cloud.ts
+++ src/pages/element-choice-cloud/element-choice-cloud.ts
@@ -17,6 +17,54 @@ export class ElementChoiceCloudPage {
 
   ionViewDidLoad() {
     console.log('Hello ElementChoiceCloudPage Page');
+
+    function nextButton() { 
+    // A ADAPTER EN TYPE SCRIPT, CA NE MARCHE PAS .........
+    	var btn = document.querySelector(".buttonArea");
+    	console.log(btn);
+	    var clicked = false;
+	    var completed = false; 
+
+	      btn.addEventListener("mousedown", function(){
+	        document.querySelector(".water-fill").classList.add("anim");
+	        document.querySelector(".water-fill2").classList.add("anim");
+	        clicked = true;
+	      });
+	      btn.addEventListener("mouseup", function(){
+	        clicked = false;
+	        if (completed == false){
+	          document.querySelector(".water-fill").classList.remove("anim");
+	          document.querySelector(".water-fill2").classList.remove("anim");
+	        }
+	      });
+	      document.querySelector(".water-fill").addEventListener('webkitTransitionEnd', function(){
+	        if (clicked){
+	          completed = true;
+	          console.log('HERE YOU SWITCH PAGE ON PHONE AND DESKTOP -> NEXT CHAPTER');
+	        }
+	      })
+    }    
+
   }
 
 }
+
+
+class Tracker {
+    count = 0;
+    constructor() {
+        window.addEventListener("mousedown", this.mouseDown);
+        window.addEventListener("mouseup", this.mouseUp);
+    }
+    mouseDown = (ev: MouseEvent) => {
+        window.addEventListener("mousemove", this.mouseMove);
+    }
+    mouseUp = (ev: MouseEvent) => {
+        window.removeEventListener("mousemove", this.mouseMove);
+    }
+    mouseMove = (ev: MouseEvent) => {
+        this.count++;
+        console.log(this.count);
+    }
+}
+new Tracker();
